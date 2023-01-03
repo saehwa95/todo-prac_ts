49033be6106e8c759402117c6495522f31aa03b2
Saehwa95 <tpghk1002@naver.com>
2023-01-03 22:15:14 +0900

    - 타입스크립 폴더 구조 및 파일 정리 - counter 만들기 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+  const increase = () => setCount(count + 1);
+
+  const decrease = () => setCount(count - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>counter</h1>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={increase}>+1</button>
+        <button onClick={decrease}>-1</button>
+      </div>
+    </Container>
   );
-}
+};
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 300px;
+  height: 300px;
+  border: 1px solid #808080;
+  margin: auto;
+  margin-top: 100px;
+`;
