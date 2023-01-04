58b38c5f6acee840f244e2755b65221bc0827b34
Saehwa95 <tpghk1002@naver.com>
2023-01-04 14:45:16 +0900

    투두리스트 만들기 기초 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./shared/Router";
 
 const App = () => {
-  const [count, setCount] = useState<number>(0);
-  const increase = () => setCount(count + 1);
-
-  const decrease = () => setCount(count - 1);
-
   return (
-    <Container>
-      <h1>counter</h1>
-      <h1>{count}</h1>
-      <div>
-        <button onClick={increase}>+1</button>
-        <button onClick={decrease}>-1</button>
-      </div>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 };
 
 export default App;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 300px;
-  height: 300px;
-  border: 1px solid #808080;
-  margin: auto;
-  margin-top: 100px;
-`;
